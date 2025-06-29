b2587187395189285a6e4a2b586b0561a789ec45
Wang Haoyang <haoyangw@gmail.com>
2025-06-29 11:38:29 +0000

    Add new flavor to rock-5b board
    Support building the Ubuntu MATE flavor for the Radxa
      Rock 5B board.

--- config/boards/rock-5b.sh
+++ config/boards/rock-5b.sh
@@ -7,7 +7,7 @@ export BOARD_CPU="ARM Cortex A76 / A55"
 export UBOOT_PACKAGE="u-boot-radxa-rk3588"
 export UBOOT_RULES_TARGET="rock-5b-rk3588"
 export COMPATIBLE_SUITES=("jammy" "noble")
-export COMPATIBLE_FLAVORS=("server" "desktop")
+export COMPATIBLE_FLAVORS=("server" "desktop" "mate")
 
 function config_image_hook__rock-5b() {
     local rootfs="$1"
