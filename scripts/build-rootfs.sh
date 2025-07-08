9baefb76c1d9604caafdf4072ddfb86dc45864fb
Wang Haoyang <haoyangw@gmail.com>
2025-07-08 03:15:44 +0000

    Partially revert GNOME package removal logic
    Avoid removing GNOME desktop's recommended packages, since
      some of those are indirect dependencies for MATE desktop
      (and thus causes MATE desktop to get removed).
    
    Also, merge MATE desktop's dependencies and recommended packages
      to ensure that even recommended packages don't get removed
      when purging GNOME desktop packages.

--- scripts/build-rootfs.sh
+++ scripts/build-rootfs.sh
@@ -77,15 +77,12 @@ function remove_gnome() {
 
 			# List of Ubuntu (GNOME) desktop dependencies
 			gnome_deps=\$((get_deps_of ubuntu-desktop Depends; get_deps_of ubuntu-desktop-minimal Depends;) | sort -u)
-			gnome_recs=\$((get_deps_of ubuntu-desktop Recommends; get_deps_of ubuntu-desktop-minimal Recommends) | sort -u)
 
 			# List of Ubuntu flavor's desktop dependencies
-			flavor_deps=\$(get_deps_of "$flavor_desktop" Depends | sort -u)
-			flavor_recs=\$(get_deps_of "$flavor_desktop" Recommends | sort -u)
+			flavor_deps=\$((get_deps_of "$flavor_desktop" Depends; get_deps_of "$flavor_desktop" Recommends) | sort -u)
 
 			# Exclude flavor dependencies from list of GNOME dependencies to remove
 			deps_to_remove=\$(comm -23 <(echo "\$gnome_deps") <(echo "\$flavor_deps"))
-			recs_to_remove=\$(comm -23 <(echo "\$gnome_recs") <(echo "\$flavor_recs"))
 
 			if [ -n "\$deps_to_remove" ]; then
 				# Remove GNOME package dependencies
@@ -94,12 +91,6 @@ function remove_gnome() {
 
 				# Reconfigure lightdm display manager after removing gdm3
 				dpkg-reconfigure -fnoninteractive lightdm
-			fi
-
-			if [ -n "\$recs_to_remove" ]; then
-				# Remove GNOME package dependencies
-				echo "Removing GNOME recommended packages..."
-				echo "\$recs_to_remove" | xargs apt-get purge --auto-remove --yes || true
 			fi
 
 			# Remove Ubuntu GNOME metapackages
