3361e2a6e3dfccf63f156dc929f013f8eef429e3
Wang Haoyang <haoyangw@gmail.com>
2025-07-16 18:01:59 +0800

    Add support for Rock 3 boards
    Add basic build configs for Rock 3A/3B/3C boards so that
      Ubuntu for Rockchip images can be built for these boards
      as well.
    
    Kernel and U-Boot support have already been added, so Ubuntu
      should at least boot on these boards.
    
    Note that since only my Rockchip 6.1 kernel supports these boards,
      only Ubuntu Noble support is specified in their build config.

---
+++ config/boards/rock-3a.sh
@@ -0,0 +1,27 @@
+# shellcheck shell=bash
+
+export BOARD_NAME="Radxa ROCK 3A"
+export BOARD_MAKER="Radxa"
+export BOARD_SOC="Rockchip RK3568"
+export BOARD_CPU="ARM Cortex A55"
+export UBOOT_PACKAGE="u-boot-turing-rk35xx"
+export UBOOT_RULES_TARGET="rock-3a-rk3568"
+export COMPATIBLE_SUITES=("noble")
+export COMPATIBLE_FLAVORS=("server" "desktop")
+
+function config_image_hook__rock-3a() {
+    local rootfs="$1"
+    local overlay="$2"
+    local suite="$3"
+
+    if [ "${suite}" == "jammy" ] || [ "${suite}" == "noble" ]; then
+        # Install the rockchip camera engine
+        chroot "${rootfs}" apt-get -y install camera-engine-rkaiq-rk3568
+
+        # Fix Bluetooth not working with Radxa RTL8852BE WiFi + BT card
+        cp "${overlay}/usr/lib/systemd/system/radxa-a8-bluetooth.service" "${rootfs}/usr/lib/systemd/system/radxa-a8-bluetooth.service"
+        chroot "${rootfs}" systemctl enable radxa-a8-bluetooth
+    fi
+
+    return 0
+}
---
+++ config/boards/rock-3c.sh
@@ -0,0 +1,43 @@
+# shellcheck shell=bash
+
+export BOARD_NAME="Radxa ROCK 3C"
+export BOARD_MAKER="Radxa"
+export BOARD_SOC="Rockchip RK3566"
+export BOARD_CPU="ARM Cortex A55"
+export UBOOT_PACKAGE="u-boot-radxa-rk35xx"
+export UBOOT_RULES_TARGET="rock-3c-rk3566"
+export COMPATIBLE_SUITES=("noble")
+export COMPATIBLE_FLAVORS=("server" "desktop")
+
+function config_image_hook__rock-3c() {
+    local rootfs="$1"
+    local overlay="$2"
+    local suite="$3"
+
+    if [ "${suite}" == "jammy" ] || [ "${suite}" == "noble" ]; then
+        # Install the rockchip camera engine
+        chroot "${rootfs}" apt-get -y install camera-engine-rkaiq-rk3568
+
+        # Kernel modules to blacklist
+        (
+            echo "blacklist aic8800_bsp"
+            echo "blacklist aic8800_fdrv"
+            echo "blacklist aic8800_btlpm"
+        ) > "${rootfs}/etc/modprobe.d/aic8800.conf"
+
+        # Install AIC8800 SDIO WiFi and Bluetooth DKMS
+        chroot "${rootfs}" apt-get -y install dkms aic8800-firmware aic8800-sdio-dkms
+
+        # shellcheck disable=SC2016
+        echo 'SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="88:00:*", NAME="$ENV{ID_NET_SLOT}"' > "${rootfs}/etc/udev/rules.d/99-radxa-aic8800.rules"
+
+        # Enable the on-board bluetooth module AIC8800
+        mkdir -p "${rootfs}/usr/lib/scripts/"
+        cp "${overlay}/usr/bin/bt_test" "${rootfs}/usr/bin/bt_test"
+        cp "${overlay}/usr/lib/scripts/aic8800-bluetooth.sh" "${rootfs}/usr/lib/scripts/aic8800-bluetooth.sh"
+        cp "${overlay}/usr/lib/systemd/system/aic8800-bluetooth.service" "${rootfs}/usr/lib/systemd/system/aic8800-bluetooth.service"
+        chroot "${rootfs}" systemctl enable aic8800-bluetooth
+    fi
+
+    return 0
+}
