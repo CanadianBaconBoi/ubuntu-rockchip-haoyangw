cd4deda106342eff87e4a7718b05e3d186d4afe0
Wang Haoyang <haoyangw@gmail.com>
2025-07-16 18:47:09 +0800

    Add MATE flavor support to Radxa Zero 3 too
    Radxa Zero 3 also has a weak GPU like the Rock 3 boards,
      so build Ubuntu MATE images for this board as well

--- config/boards/radxa-zero3.sh
+++ config/boards/radxa-zero3.sh
@@ -7,7 +7,7 @@ export BOARD_CPU="ARM Cortex A55"
 export UBOOT_PACKAGE="u-boot-radxa-rk35xx"
 export UBOOT_RULES_TARGET="radxa-zero3-rk3566"
 export COMPATIBLE_SUITES=("jammy" "noble")
-export COMPATIBLE_FLAVORS=("server" "desktop")
+export COMPATIBLE_FLAVORS=("server" "desktop" "mate")
 
 function config_image_hook__radxa-zero3() {
     local rootfs="$1"
